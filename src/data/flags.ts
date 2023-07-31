fde8a062514f35fc6e6aa77e2f1796a2b946d29c
opeyem1a <opeyadeyemi@gmail.com>
2023-07-30 23:21:06 -0600

    turn on new project rows

--- src/data/flags.ts
+++ src/data/flags.ts
@@ -1,6 +1,6 @@
 export const FLAG_NEW_PROJECT_ROWS = "FLAG_NEW_PROJECT_ROWS"
 export const FLAGS = {
-    FLAG_NEW_PROJECT_ROWS: false,
+    FLAG_NEW_PROJECT_ROWS: true,
 }
 
 export const isActive = (flagName: keyof typeof FLAGS) => {
